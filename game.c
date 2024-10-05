9205c6725bcfd48d8bcc08eeb9dc6182e1902126
Steve SCHNEPP <steve.schnepp@pwkf.org>
2024-10-05 16:34:20 +0200

    adding sprite

---
+++ game.c
@@ -0,0 +1,14 @@
+#include "usdl.h"
+
+float randf(float max) {
+    float r = rand();
+    r = r * max / RAND_MAX;
+    return r;
+}
+
+void draw() {
+    cls();
+    for (int i = 0; i < 200; i++) {
+        spr_zoom(0, randf(SCREEN_WIDTH), randf(SCREEN_HEIGHT), 0.1, 0.1);
+    }
+}
