9b5be439477eb757ac67bdbf50fc389fd2a37ac7
Steve Schnepp <steve.schnepp@pwkf.org>
2024-10-06 17:20:58 +0200

    adding pico8 style api

--- p8sdl/p8sdl.h
+++ p8sdl/p8sdl.h
@@ -24,15 +24,27 @@ enum draw_keepscreen {
 	UDSL_CLEARSCREEN = 1,
 };
 
+/* PICO-8 style API */
+
 void cls();
 void draw();
 void update(uint32_t delta_ms);
-void setup(int argc, char *argv[]);
+
+void spr(int id, float x, float y);
+
+void rect(float x1, float y1, float x2, float y2, int color);
+void rectfill(float x1, float y1, float x2, float y2, int color);
+
+void circ(float x, float y, float r, int color);
+void circfill(float x, float y, float r, int color);
 
 int btn(int id, int player_id);
 int btnp(int id, int player_id);
 
-void screenshot();
+void print(char *msg, float x, float y, int color);
 
-void spr(int id, float x, float y);
+/* PICO-8 style API extensions */
 void spr_zoom(int id, float x, float y, float zx, float zy);
+
+void setup(int argc, char *argv[]);
+void screenshot();
